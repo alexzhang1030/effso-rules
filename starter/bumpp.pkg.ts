54a29224448ab002bc62bce631bc2f9fdf525935
alexzhang1030 <1642114555@qq.com>
2023-05-11 10:09:57 +0800

    feat(rules): add tsup

--- starter/bumpp.pkg.ts
+++ starter/bumpp.pkg.ts
@@ -2,7 +2,7 @@ function main(pkg: Record<string, any>) {
   return {
     ...pkg,
     scripts: {
-      ...pkg,
+      ...pkg.scripts,
       "prepublishOnly": "pnpm run build",
       "release": "bumpp --commit \"chore: release v%s\" --push --tag && pnpm publish"
     }
---
+++ starter/tsup.file.ts
@@ -0,0 +1,18 @@
+function main(path: string, helpers: {
+  read: (path: string) => string
+  write: (path: string, content: string) => void
+}) {
+  const target = `${path}/tsup.config.ts`
+  const content = helpers.read(target)
+  if (!content) {
+    helpers.write(target, `
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  test: {
+    includeSource: ['src/*'],
+    globals: true,
+  },
+})`)}
+}
+
