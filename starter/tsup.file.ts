50323ff791c95d3ea304efbef6aa9384253a5ed2
alexzhang1030 <1642114555@qq.com>
2023-07-31 21:24:10 +0800

    feat: add tsup

---
+++ starter/tsup.file.ts
@@ -0,0 +1,25 @@
+function main(
+  path: string,
+  helpers: {
+    read: (path: string) => string;
+    write: (path: string, content: string) => void;
+  }
+) {
+  const target = `${path}/uno.config.ts`;
+  const content = helpers.read(target);
+  if (!content) {
+    helpers.write(
+      target,
+      `import { defineConfig } from 'tsup'
+
+export default defineConfig({
+  clean: true,
+  entry: ['src/index.ts'],
+  target: 'esnext',
+  format: ['esm', 'cjs'],
+  dts: true,
+})
+`
+    );
+  }
+}
