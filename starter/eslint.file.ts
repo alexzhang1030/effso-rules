1c01b3415f2fec7dbef866a96fe8928f9c4ff557
alexzhang1030 <1642114555@qq.com>
2023-11-02 10:23:57 +0800

    chore: use stylistic to format

---
+++ starter/eslint.file.ts
@@ -0,0 +1,22 @@
+const text = `import antfu from '@antfu/eslint-config'
+
+export default antfu({
+  typescript: {
+    tsconfigPath: "tsconfig.json"
+  }
+})
+`;
+
+function main(
+  path: string,
+  helpers: {
+    read: (path: string) => string;
+    write: (path: string, content: string) => void;
+  },
+) {
+  const target = `${path}/eslint.config.js`;
+  const content = helpers.read(target);
+  if (!content) {
+    helpers.write(target, text);
+  }
+}
--- starter/use_eslint.file.ts
+++ starter/use_eslint.file.ts
@@ -1,23 +1,60 @@
-function main(path: string, helpers: {
-  read: (path: string) => string
-  write: (path: string, content: string) => void
-  merge: (target: object, source: object) => object
-}) {
-  const target = `${path}/.vscode/settings.json`
-  const content = helpers.read(target)
+function main(
+  path: string,
+  helpers: {
+    read: (path: string) => string;
+    write: (path: string, content: string) => void;
+    merge: (target: object, source: object) => object;
+  },
+) {
+  const target = `${path}/.vscode/settings.json`;
+  const content = helpers.read(target);
 
   const settings = {
-   "prettier.enable": false,
-   "editor.codeActionsOnSave": {
-     "source.fixAll.eslint": true
-   },
-   "editor.formatOnSave": false
-  }
+    // Enable the ESlint flat config support
+    "eslint.experimental.useFlatConfig": true,
+
+    // Disable the default formatter, use eslint instead
+    "prettier.enable": false,
+    "editor.formatOnSave": false,
+
+    // Auto fix
+    "editor.codeActionsOnSave": {
+      "source.fixAll.eslint": "explicit",
+      "source.organizeImports": "never",
+    },
+
+    // Silent the stylistic rules in you IDE, but still auto fix them
+    "eslint.rules.customizations": [
+      { rule: "style/*", severity: "off" },
+      { rule: "*-indent", severity: "off" },
+      { rule: "*-spacing", severity: "off" },
+      { rule: "*-spaces", severity: "off" },
+      { rule: "*-order", severity: "off" },
+      { rule: "*-dangle", severity: "off" },
+      { rule: "*-newline", severity: "off" },
+      { rule: "*quotes", severity: "off" },
+      { rule: "*semi", severity: "off" },
+    ],
+
+    // Enable eslint for all supported languages
+    "eslint.validate": [
+      "javascript",
+      "javascriptreact",
+      "typescript",
+      "typescriptreact",
+      "vue",
+      "html",
+      "markdown",
+      "json",
+      "jsonc",
+      "yaml",
+    ],
+  };
 
   if (!content || !content.trim().length) {
-    helpers.write(target, JSON.stringify(settings, null, 2))
+    helpers.write(target, JSON.stringify(settings, null, 2));
   } else {
-    const fileContent = helpers.merge(settings, JSON.parse(content))
-    helpers.write(target, JSON.stringify(fileContent, null, 2))
+    const fileContent = helpers.merge(settings, JSON.parse(content));
+    helpers.write(target, JSON.stringify(fileContent, null, 2));
   }
 }
