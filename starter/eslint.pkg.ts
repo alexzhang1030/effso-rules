b26ea53827427251dead580bea35fb13dd20056b
alexzhang1030 <1642114555@qq.com>
2023-11-02 10:29:05 +0800

    chore: update

--- starter/eslint.pkg.ts
+++ starter/eslint.pkg.ts
@@ -1,21 +1,16 @@
 function main(pkg: Record<string, any>) {
   return {
     ...pkg,
-    "scripts": {
+    scripts: {
       ...pkg.scripts,
-      "lint": "eslint .",
-      "prepare": "simple-git-hooks"
+      lint: "eslint .",
+      postinstall: "simple-git-hooks",
     },
     "simple-git-hooks": {
-      "pre-commit": "pnpm exec lint-staged"
+      "pre-commit": "pnpm exec lint-staged",
     },
     "lint-staged": {
-      "*.{js,ts,json,md,yaml,yml}": [
-        "eslint --fix"
-      ]
+      "*.{js,ts,json,md,yaml,yml}": ["eslint --fix"],
     },
-    "eslintConfig": {
-      "extends": "@antfu/eslint-config-ts"
-    },
-  }
+  };
 }
